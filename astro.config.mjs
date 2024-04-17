5ad97e3f702a49bc60b7d539a181a5ce9885c40c
Jorge Alfaro <jorgealfaro@grupoicarus.com.mx>
2024-04-17 14:54:38 -0600

    add vercel  adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import preact from "@astrojs/preact";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro.alfaro.pw",
-  integrations: [preact()]
+  integrations: [preact()],
+  output: "server",
+  adapter: vercel()
 });
